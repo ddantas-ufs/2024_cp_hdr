41dec07e1c62a07a50e5165c20e321169a98bdbd
isacalenc <isac.silva@dcomp.ufs.br>
2021-03-03 23:57:42 -0300

    fixed MAX_SUP value and output format of kp

--- include/detectors/core.h
+++ include/detectors/core.h
@@ -10,18 +10,18 @@
 
 #define NUM_OCTAVES 4
 #define NUM_SCALES 5
-#define MAX_INTERP_STEPS 5 //interpolation max steps before failure. Based on OpenSIFT and OpenCV
+#define MAX_INTERP_STEPS 5 //interpolation max steps before failure (based on OpenSIFT and OpenCV)
 #define DOG_BORDER 5 //interpolation border to ignore keypoints
 #define CONTRAST_TH 0.03 //prybil set to 8
 #define CURV_TH 5
 #define GAUSS_SIZE 9
 #define SIGMA_X 0 //fix value (ex. 1.0) to keep a standard
 #define SIGMA_Y 0 //fix value (ex. 1.0) to keep a standard
-#define MAXSUP_SIZE 5
+#define MAXSUP_SIZE 21 //can be 3 (based on Lowe's paper)
 #define SOBEL_SIZE 7
 #define K 0.04
 #define MIN_QUALITY 0.05
-#define MAX_KP 0
+#define MAX_KP 500 //if zero, it is not used
 #define CV_SIZE 3 //can be 5 to harris
 
 struct KeyPoints
