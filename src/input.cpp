d90ac07b54ed4e9d4a5b24618ea977fcf8dd0b3e
isacalenc <isac.silva@dcomp.ufs.br>
2021-03-12 00:24:41 -0300

    fixed read of hdr and ldr images

--- src/input.cpp
+++ src/input.cpp
@@ -3,14 +3,22 @@
 void readImg(char *img_path, cv::Mat &img_in, cv::Mat &img_gray, std::string &img_name)
 {
   img_in = cv::imread(img_path, cv::IMREAD_UNCHANGED);
-  
+
   if (img_in.channels() != 1)
   {
     cv::cvtColor(img_in, img_gray, cv::COLOR_BGR2GRAY);
   }
-  if (img_in.depth() == CV_32F)
+  else
   {
-    cv::normalize(img_in, img_gray, 0.0, 256.0, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    img_gray = img_in;
+  }
+  if (img_gray.depth() == CV_32F)
+  {
+    cv::normalize(img_gray, img_gray, 0.0, 256.0, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+  }
+  else
+  {
+    cv::normalize(img_gray, img_gray, 0, 255, cv::NORM_MINMAX, CV_8UC1, cv::Mat());
   }
   img_name = getFileName(std::string(img_path));
 }
@@ -18,7 +26,7 @@ void readImg(char *img_path, cv::Mat &img_in, cv::Mat &img_gray, std::string &im
 std::string getFileName(std::string file_path)
 {
   size_t size = file_path.rfind("/", file_path.length());
-  	
+
   if (size != std::string::npos)
   {
     file_path = file_path.substr(size + 1, file_path.length() - size);
@@ -28,7 +36,7 @@ std::string getFileName(std::string file_path)
     file_path = "";
   }
   size = file_path.rfind(".", file_path.length());
-  	
+
   if (size != std::string::npos)
   {
     return file_path.substr(0, size);
