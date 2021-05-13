972c7d702b02c8fa31e18a4630ea47e4492c0f27
Artur Santos Nascimento <arturxz@outlook.com.br>
2021-05-13 00:39:04 -0300

    started refactoring

--- src/sift.cpp
+++ src/sift.cpp
@@ -658,33 +658,44 @@ void siftExecuteDescription( std::vector<KeyPoints> &kpList, cv::Mat &img )
 }
 
 //*
-//calcSIFTDescriptor(img, kp, angle, size*0.5f, d, n, descriptors, i);
-void calcSIFTDescriptor( cv::Mat &img, KeyPoints &ptf, float ori, float scl,
-                         int d, int n, float* dst )
-{
-  cv::Point pt(cvRound(ptf.x), cvRound(ptf.y));
-  float cos_t = cosf(ori*(float)(CV_PI/180));
-  float sin_t = sinf(ori*(float)(CV_PI/180));
-  float bins_per_rad = n / 360.f;
-  float exp_scale = -1.f/(d * d * 0.5f);
-  float hist_width = SIFT_DESC_SCL_FTR * scl;
-  int radius = cvRound(hist_width * 1.4142135623730951f * (d + 1) * 0.5f);
-  cos_t /= hist_width;
-  sin_t /= hist_width;
 
-  int i, j, k, len = (radius*2+1)*(radius*2+1), histlen = (d+2)*(d+2)*(n+2);
+//calcDescriptor( img, kpl[i], angle, size*0.5f, SIFT_DESC_SW_QTD,
+//                SIFT_DESC_BINS_PER_SW, descriptor);
+//void calcDescriptor( cv::Mat &img, KeyPoints &ptf, float ori, float scl,
+//                     int d, int n, float* dst )
+void calcDescriptor( cv::Mat &img, KeyPoints &ptf, float angle, float scale, float* descriptor )
+{
+  int qtdSW = SIFT_DESC_SW_QTD, binsPerSW = SIFT_DESC_BINS_PER_SW;
+
+  // get sin and cos to rotate angle
+  float angleCos = cosf(angle*(float)(M_PI/180));
+  float angleSin = sinf(angle*(float)(M_PI/180));
+  float bins_per_rad = binsPerSW / 360.f;
+  float exp_scale = -1.f/( qtdSW*qtdSW*0.5f );
+
+  // radius size according to keypoint's scale
+  float hist_width = SIFT_DESC_SCL_FTR * scale;
+  int radius = cvRound(hist_width * sqrtf(2) * (qtdSW + 1) * 0.5f);
+
+  // adjusting sin and cos to 
+  angleCos /= hist_width;
+  angleSin /= hist_width;
+
+  int i, j, k, len = (radius*2+1)*(radius*2+1), histlen = (qtdSW+2)*(qtdSW+2)*(binsPerSW+2);
   int rows = img.rows, cols = img.cols;
 
-  cv::AutoBuffer<float> buf(len*6 + histlen);
-  float *X = buf, *Y = X + len, *Mag = Y, *Ori = Mag + len, *W = Ori + len;
-  float *RBin = W + len, *CBin = RBin + len, *hist = CBin + len;
-  //float X, Y, Mag, Ori, W, RBin, CBin, hist;
+  //cv::AutoBuffer<float> buf(len*6 + histlen);
+  //float *X = buf, *Y = X + len, *Mag = Y, *Ori = Mag + len, *W = Ori + len;
+  //float *RBin = W + len, *CBin = RBin + len, *hist = CBin + len;
+  float arr_dx[len], arr_dy[len], arr_angle[len], W[len], arr_rbin[len], arr_cbin[len];
+  float hist[histlen];
+  float *Mag = arr_dy;
 
-  for( i = 0; i < d+2; i++ )
+  for( i = 0; i < qtdSW+2; i++ )
   {
-    for( j = 0; j < d+2; j++ )
-      for( k = 0; k < n+2; k++ )
-        hist[(i*(d+2) + j)*(n+2) + k] = 0.;
+    for( j = 0; j < qtdSW+2; j++ )
+      for( k = 0; k < binsPerSW+2; k++ )
+        hist[(i*(qtdSW+2) + j)*(binsPerSW+2) + k] = 0.f;
   }
 
   for( i = -radius, k = 0; i <= radius; i++ )
@@ -693,32 +704,32 @@ void calcSIFTDescriptor( cv::Mat &img, KeyPoints &ptf, float ori, float scl,
       // Calculate sample's histogram array coords rotated relative to ori.
       // Subtract 0.5 so samples that fall e.g. in the center of row 1 (i.e.
       // r_rot = 1.5) have full weight placed in row 1 after interpolation.
-      float c_rot = j * cos_t - i * sin_t;
-      float r_rot = j * sin_t + i * cos_t;
-      float rbin = r_rot + d/2 - 0.5f;
-      float cbin = c_rot + d/2 - 0.5f;
-      int r = pt.y + i, c = pt.x + j;
+      float c_rot = j * angleCos - i * angleSin;
+      float r_rot = j * angleSin + i * angleCos;
+      float rbin = r_rot + qtdSW/2 - 0.5f;
+      float cbin = c_rot + qtdSW/2 - 0.5f;
+      int r = ptf.y + i, c = ptf.x + j;
 
-      if( rbin > -1 && rbin < d && cbin > -1 && cbin < d &&
+      if( rbin > -1 && rbin < qtdSW && cbin > -1 && cbin < qtdSW &&
           r > 0 && r < rows - 1 && c > 0 && c < cols - 1 )
       {
         float dx = (float)(img.at<float>(r, c+1) - img.at<float>(r, c-1));
         float dy = (float)(img.at<float>(r-1, c) - img.at<float>(r+1, c));
-        X[k] = dx; Y[k] = dy; RBin[k] = rbin; CBin[k] = cbin;
+        arr_dx[k] = dx; arr_dy[k] = dy; arr_rbin[k] = rbin; arr_cbin[k] = cbin;
         W[k] = (c_rot * c_rot + r_rot * r_rot)*exp_scale;
         k++;
       }
     }
 
   len = k;
-  cv::hal::fastAtan2(Y, X, Ori, len, true);
-  cv::hal::magnitude(X, Y, Mag, len);
+  cv::hal::fastAtan2(arr_dy, arr_dx, arr_angle, len, true);
+  cv::hal::magnitude(arr_dx, arr_dy, Mag, len);
   cv::hal::exp(W, W, len);
 
   for( k = 0; k < len; k++ )
   {
-    float rbin = RBin[k], cbin = CBin[k];
-    float obin = (Ori[k] - ori)*bins_per_rad;
+    float rbin = arr_rbin[k], cbin = arr_cbin[k];
+    float obin = (arr_angle[k] - angle)*bins_per_rad;
     float mag = Mag[k]*W[k];
 
     int r0 = cvFloor( rbin );
@@ -729,9 +740,9 @@ void calcSIFTDescriptor( cv::Mat &img, KeyPoints &ptf, float ori, float scl,
     obin -= o0;
 
     if( o0 < 0 )
-        o0 += n;
-    if( o0 >= n )
-        o0 -= n;
+        o0 += binsPerSW;
+    if( o0 >= binsPerSW )
+        o0 -= binsPerSW;
 
     // histogram update using tri-linear interpolation
     float v_r1 = mag*rbin, v_r0 = mag - v_r1;
@@ -742,110 +753,81 @@ void calcSIFTDescriptor( cv::Mat &img, KeyPoints &ptf, float ori, float scl,
     float v_rco011 = v_rc01*obin, v_rco010 = v_rc01 - v_rco011;
     float v_rco001 = v_rc00*obin, v_rco000 = v_rc00 - v_rco001;
 
-    int idx = ((r0+1)*(d+2) + c0+1)*(n+2) + o0;
+    int idx = ((r0+1)*(qtdSW+2) + c0+1)*(binsPerSW+2) + o0;
     hist[idx] += v_rco000;
     hist[idx+1] += v_rco001;
-    hist[idx+(n+2)] += v_rco010;
-    hist[idx+(n+3)] += v_rco011;
-    hist[idx+(d+2)*(n+2)] += v_rco100;
-    hist[idx+(d+2)*(n+2)+1] += v_rco101;
-    hist[idx+(d+3)*(n+2)] += v_rco110;
-    hist[idx+(d+3)*(n+2)+1] += v_rco111;
+    hist[idx+(binsPerSW+2)] += v_rco010;
+    hist[idx+(binsPerSW+3)] += v_rco011;
+    hist[idx+(qtdSW+2)*(binsPerSW+2)] += v_rco100;
+    hist[idx+(qtdSW+2)*(binsPerSW+2)+1] += v_rco101;
+    hist[idx+(qtdSW+3)*(binsPerSW+2)] += v_rco110;
+    hist[idx+(qtdSW+3)*(binsPerSW+2)+1] += v_rco111;
   }
 
   // finalize histogram, since the orientation histograms are circular
-  for( i = 0; i < d; i++ )
-    for( j = 0; j < d; j++ )
+  for( i = 0; i < qtdSW; i++ )
+    for( j = 0; j < qtdSW; j++ )
     {
-      int idx = ((i+1)*(d+2) + (j+1))*(n+2);
-      hist[idx] += hist[idx+n];
-      hist[idx+1] += hist[idx+n+1];
-      for( k = 0; k < n; k++ )
-        dst[(i*d + j)*n + k] = hist[idx+k];
-      }
-  // copy histogram to the descriptor,
-  // apply hysteresis thresholding
-  // and scale the result, so that it can be easily converted
-  // to byte array
+      int idx = ((i+1)*(qtdSW+2) + (j+1))*(binsPerSW+2);
+      hist[idx] += hist[idx+binsPerSW];
+      hist[idx+1] += hist[idx+binsPerSW+1];
+      for( k = 0; k < binsPerSW; k++ )
+        descriptor[(i*qtdSW + j)*binsPerSW + k] = hist[idx+k];
+    }
+  
+  // copy histogram to the descriptor, apply hysteresis thresholding
+  // and scale the result, so that it can be easily converted to byte array
   float nrm2 = 0;
-  len = d*d*n;
+  len = qtdSW*qtdSW*binsPerSW;
   for( k = 0; k < len; k++ )
-      nrm2 += dst[k]*dst[k];
+    nrm2 += descriptor[k]*descriptor[k];
+
   float thr = std::sqrt(nrm2)*SIFT_DESC_MAG_THR;
   for( i = 0, nrm2 = 0; i < k; i++ )
   {
-    float val = std::min(dst[i], thr);
-    dst[i] = val;
+    float val = std::min(descriptor[i], thr);
+    descriptor[i] = val;
     nrm2 += val*val;
   }
   nrm2 = SIFT_DESC_INT_FTR/std::max(std::sqrt(nrm2), FLT_EPSILON);
 
-#if 1
   for( k = 0; k < len; k++ )
-  {
-    dst[k] = uchar( dst[k]*nrm2 );// saturate_cast<uchar>(dst[k]*nrm2);
-  }
-#else
-  float nrm1 = 0;
-  for( k = 0; k < len; k++ )
-  {
-    dst[k] *= nrm2;
-    nrm1 += dst[k];
-  }
-  nrm1 = 1.f/std::max(nrm1, FLT_EPSILON);
-  for( k = 0; k < len; k++ )
-  {
-    dst[k] = std::sqrt(dst[k] * nrm1);//saturate_cast<uchar>(std::sqrt(dst[k] * nrm1)*SIFT_INT_DESCR_FCTR);
-  }
-#endif
+    descriptor[k] = uchar( descriptor[k]*nrm2 );
 }
 
-void unpackOpenCVOctave( cv::KeyPoint& kp, int& octave, int& layer, float& scale)
+void unpackOpenCVOctave( cv::KeyPoint &kp, int &octave, int &layer, float &scale)
 {
   octave = kp.octave & 255;
   layer = (kp.octave >> 8) & 255;
   octave = octave < 128 ? octave : (-128 | octave);
   scale = octave >= 0 ? 1.f/(1 << octave) : (float)(1 << -octave);
 }
-
-
-//void calcDescriptors(const vector<Mat>& gpyr, const vector<KeyPoint>& keypoints,
-//                          Mat& descriptors, int nOctaveLayers, int firstOctave )
+/**
+ * 
+**/
 void calcDescriptors(std::vector<KeyPoints> &kpl, cv::Mat &img )
 {
-  int d = SIFT_DESC_SW_QTD, n = SIFT_DESC_BINS_PER_SW;
-
-  //std::vector<float> descriptors;
-  //descriptors.resize(SIFT_DESC_SIZE);
-  //float descriptor[SIFT_DESC_SIZE];
-  //descriptors.resize(SIFT_DESC_SIZE);
-
+  // FOR EACH KP IN LIST...
   for ( int i=0; i<kpl.size(); i++ )
   {
     float descriptor[SIFT_DESC_SIZE];
-    //KeyPoints kp = kpl[i];
     kpl[i].descriptor.resize(SIFT_DESC_SIZE);
-    //for( int k = 0; k < SIFT_DESC_SIZE; k++ )
-    //  descriptor[k] = 0.0f;
 
-    //int octave, layer;
-    //float scale;
-    //unpackOpenCVOctave(kpt, octave, layer, scale);
-    //CV_Assert(octave >= firstOctave && layer <= NUM_SCALES+2);
     float size = DOG_BORDER*kpl[i].scale;
-    //cv::Point2f ptf(kpt.x*kpt.scale, kpt.y*kpt.scale);
-    //const Mat& img = gpyr[(octave - firstOctave)*(NUM_OCTAVES + 3) + layer];
 
-    float angle = 360.f - kpl[i].direction;
-    if(std::abs(angle - 360.f) < FLT_EPSILON)
-        angle = 0.f;
-    calcSIFTDescriptor(img, kpl[i], angle, size*0.5f, d, n, descriptor);
+    float angle;
+    if(std::abs(kpl[i].direction) < FLT_EPSILON)
+      angle = 0.f;
+    else
+      angle = angle = 360.f - kpl[i].direction;
+
+    calcDescriptor( img, kpl[i], angle, size*0.5f, descriptor);
 
     std::cout << "Keypoint: " << i << std::endl;
     for( int k = 0; k < SIFT_DESC_SIZE; k++ )
     {
       kpl[i].descriptor[k] = descriptor[k];
-      std::cout << descriptor[k] << " ";
+      //std::cout << descriptor[k] << " ";
     }
     std::cout << std::endl;
   }
